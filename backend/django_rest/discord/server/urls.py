400b5f29f4ca4f1b18d375cd7c67f85ac184c88f
Mayntu <daniilkhusnullin@gmail.com>
2024-04-09 23:46:43 +0500

    изменение теста

--- backend/django_rest/discord/server/urls.py
+++ backend/django_rest/discord/server/urls.py
@@ -3,6 +3,6 @@ from server.views import api_auth, api_reg
 
 
 urlpatterns : list = [
-    path("/api/v1/authorization/", api_auth, name="api_auth"),
-    path("/api/v1/registration/", api_reg, name="api_reg"),
+    path("api/v1/authorization/", api_auth, name="api_auth"),
+    path("api/v1/registration/", api_reg, name="api_reg"),
 ]
