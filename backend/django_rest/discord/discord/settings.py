81cf0029abf47a7e2ac765257cd8f6d25e478860
Mayntu <daniilkhusnullin@gmail.com>
2024-04-10 23:46:32 +0500

    test

--- backend/django_rest/discord/discord/settings.py
+++ backend/django_rest/discord/discord/settings.py
@@ -46,20 +46,21 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'http://localhost:5173',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://127.0.0.1:8000',
+#     'http://127.0.0.1:5173',
+#     'http://127.0.0.1:5500',
+# ]
 
 ROOT_URLCONF = 'discord.urls'
 
