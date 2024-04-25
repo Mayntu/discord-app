efc7158b2da100f639313769ac05e478e16ca5ef
Blaze220 <132453473+Blaze220@users.noreply.github.com>
2024-04-25 08:45:55 +0500

    ц

--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/redux-hoock'
 import { fetchMedia, fetchTest } from '../store/acthion'
+import avatar from "C:\Users\1\Desktop\homework\diplom\discord-app\media\images\a7ebd92f-4355-4164-b180-49e9a7e244a4.png"
+const module = import.meta.glob("./media/images/a7ebd92f-4355-4164-b180-49e9a7e244a4.png")
 
 const Test=()=> {
+  // console.log(module)
     const [nValue,setInValue] = useState("")
     const [file,setFile] = useState()
     const dispatch =useAppDispatch()
@@ -18,9 +21,13 @@ const Test=()=> {
       formData.append("file",file[0])
       dispatch(fetchMedia(formData))
     }
+    //media\images\a7ebd92f-4355-4164-b180-49e9a7e244a4.png
+    //C:\Users\1\Desktop\homework\diplom\discord-app\media\images\a7ebd92f-4355-4164-b180-49e9a7e244a4.png
   return (
     <div className='test'>
         Test
+        
+        <img src={avatar} alt="" />
         <input type="file" accept='image/*,.png,.web,.jpg,.gif' onChange={(e)=>{
           setFile(e.target.files)
        
