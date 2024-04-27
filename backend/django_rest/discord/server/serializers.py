20c90ffbcd865abc13970c0bfd917118e07bdd02
Mayntu <daniilkhusnullin@gmail.com>
2024-04-27 18:37:34 +0500

    system of media

--- backend/django_rest/discord/server/serializers.py
+++ backend/django_rest/discord/server/serializers.py
@@ -12,14 +12,6 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ("uuid", "from_user_id", "chat_id", "content", "media", "timestamp")
-    
-
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        if instance.media:
-            representation['media'] = instance.media.url
-        return representation
 
 
 
