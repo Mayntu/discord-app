a3d60f08ece75fdebd4392fc23c876a960605df8
Mayntu <daniilkhusnullin@gmail.com>
2024-04-27 19:53:25 +0500

    test of disconnect

--- backend/flask_sockets/main.py
+++ backend/flask_sockets/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
 # from flask_cors import CORS
 from json import dumps, loads
@@ -19,7 +19,10 @@ ONLINE_USERS : list = []
 def user_connected(data):
     token : str = data.get("token")
     make_user_online(token)
-    ONLINE_USERS.append(token)
+    if not session["token"]:
+        ONLINE_USERS.append(token)
+        session["token"] = token
+    
     emit("connected", {"data" : ONLINE_USERS})
 
 
@@ -27,6 +30,10 @@ def user_connected(data):
 @socketio.on("disconnect")
 def user_disconnected():
     print("disconnected")
+    token : str = session.get("token")
+    user = ONLINE_USERS.pop(ONLINE_USERS.index(token))
+    session["token"] = None
+    
     emit("disconnected", {"data" : "user disconnected"})
 
 
