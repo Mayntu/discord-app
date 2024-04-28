92ceb4581964e18c7c45bf10a1fb622f01e1a168
Mayntu <daniilkhusnullin@gmail.com>
2024-04-28 14:04:49 +0500

    api get server room messages

--- backend/django_rest/discord/server/urls.py
+++ backend/django_rest/discord/server/urls.py
@@ -17,6 +17,7 @@ from server.views import (
     api_create_server_chat,
     api_get_server_chat_rooms,
     api_save_server_chat_message,
+    api_get_server_room_messages
 )
 
 
@@ -38,4 +39,5 @@ urlpatterns : list = [
     path("api/v1/createServerChat", api_create_server_chat, name="api_create_server_chat"),
     path("api/v1/getServerChatRooms", api_get_server_chat_rooms, name="api_get_server_chat_rooms"),
     path("api/v1/serverChatMessageSave", api_save_server_chat_message, name="api_save_server_chat_message"),
+    path("api/v1/getServerChatRoomMessages", api_get_server_room_messages, name="api_get_server_room_messages"),
 ]
