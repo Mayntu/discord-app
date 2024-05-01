e5781e4fa45e4884f51513bc60e244fae5e607fd
Mayntu <daniilkhusnullin@gmail.com>
2024-05-01 13:52:58 +0500

    fix of users and admin

--- backend/django_rest/discord/server/admin.py
+++ backend/django_rest/discord/server/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from server.models import User, Message, Chat
+from server.models import User, Message, Chat, Server, ServerMessage, ServerChatRoom
 
 
 admin.site.register(User)
 admin.site.register(Message)
 admin.site.register(Chat)
+admin.site.register(Server)
+admin.site.register(ServerMessage)
+admin.site.register(ServerChatRoom)
