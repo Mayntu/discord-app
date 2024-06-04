03521783db053b32e1c2ed0c6dabc50616473605
Mayntu <daniilkhusnullin@gmail.com>
2024-06-05 00:28:55 +0500

    info about user on sockets left join

--- backend/flask_sockets/main.py
+++ backend/flask_sockets/main.py
@@ -109,6 +109,7 @@ def join(data):
         chat_id=data.get("chat_id")
     )
     emit("join", {"users_data" : users_data})
+    emit("user-joined", {"user_status" : True}, room=data.get("chat_id"), include_self=False)
     send(message=users_data, room=data.get("chat_id"))
 
 
@@ -127,7 +128,7 @@ def join_server_chat(data):
 def leave(data):
     chat_id : str = data.get("chat_id")
     leave_room(chat_id)
-    emit("user-left", {"user_status" : "user-left"}, room=data.get("chat_id"))
+    emit("user-left", {"user_status" : False}, room=data.get("chat_id"), include_self=False)
     send(message="new user left the room", room=data["chat_id"])
 
 
